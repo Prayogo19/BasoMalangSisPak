dc3fc84f9cfc8be41a955029c64a321f74178c48
Prayogo19 <50326471+Prayogo19@users.noreply.github.com>
2019-05-07 00:41:36 +0700

    Create datauser.php

---
+++ admin/datauser.php
@@ -0,0 +1,133 @@
+<?php 
+	session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dokter - Dashboard</title>
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../assets/css/datepicker3.css" rel="stylesheet">
+	<link href="../assets/css/styles.css" rel="stylesheet">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="#"><span>SIPARTUS &nbsp;</span>Sistem Pakar Pendeteksi Ancaman Abortus</a>
+			</div>
+		</div><!-- /.container-fluid -->
+	</nav>
+	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<div class="profile-sidebar">
+			<div class="profile-userpic">
+				<img src="../assets/img/profile.png" class="img-responsive" alt="">
+			</div>
+			<div class="profile-usertitle">
+				<div class="profile-usertitle-name">Username</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="divider"></div>
+		<form role="search">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+		</form>
+		<ul class="nav menu">
+			<li ><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li class="active"><a href="datauser.php"><em class="fa fa-user">&nbsp;</em> Data User</a></li>
+			<li><a href="gejala.php"><em class="fa fa-eye">&nbsp;</em> Gejala</a></li>
+			<li><a href="penyakit.php"><em class="fa fa-bug">&nbsp;</em> Penyakit</a></li>
+			<li><a href="../logout.php"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
+		</ul>
+	</div><!--/.sidebar-->
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Data User</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Data User</h1>
+			</div>
+		</div><!--/.row-->
+		
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Tanggal Lahir</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Diagnosa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            require_once "../koneksi.php";
+
+                            $result = mysqli_query($koneksi, "SELECT id,username,nama,tanggal_lahir,alamat FROM user WHERE level='user'") or die(mysqli_error($koneksi));
+                            while ($baris = mysqli_fetch_array($result)) { ?>
+                            <tr>
+                                <td><?php echo $baris['id'] ?></td>
+                                <td><?php echo $baris['username'] ?></td>
+                                <td><?php echo $baris['nama'] ?></td>
+                                <td><?php echo $baris['tanggal_lahir'] ?></td>
+                                <td><?php echo $baris['alamat'] ?></td>
+                                <td> <a href="#"><button class="btn btn-success btn-md">Cek</button></a> </td>
+                            </tr>
+                            <?php
+                            }
+                            
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+		
+	</div>	<!--/.main-->
+	
+	<script src="../assets/js/jquery-1.11.1.min.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
+	<script src="../assets/js/chart.min.js"></script>
+	<script src="../assets/js/chart-data.js"></script>
+	<script src="../assets/js/easypiechart.js"></script>
+	<script src="../assets/js/easypiechart-data.js"></script>
+	<script src="../assets/js/bootstrap-datepicker.js"></script>
+	<script src="../assets/js/custom.js"></script>
+	<script>
+		window.onload = function () {
+	var chart1 = document.getElementById("line-chart").getContext("2d");
+	window.myLine = new Chart(chart1).Line(lineChartData, {
+	responsive: true,
+	scaleLineColor: "rgba(0,0,0,.2)",
+	scaleGridLineColor: "rgba(0,0,0,.05)",
+	scaleFontColor: "#c5c7cc"
+	});
+};
+	</script>
+		
+</body>
+</html>
