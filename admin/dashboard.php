faa303dc689670e2381ec48a7a6451494fb4159d
Prayogo19 <50326471+Prayogo19@users.noreply.github.com>
2019-05-07 00:29:54 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,20 +1,9 @@
-<?php
-session_start();
-//cek apakah user sudah login
-if(!isset($_SESSION['username'])){
-    die("Anda belum login");//jika belum login jangan lanjut
-}
-//cek level user
-if($_SESSION['level']!="admin"){
-    die("Anda bukan admin");//jika bukan admin jangan lanjut
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Lumino - Dashboard</title>
+	<title>Dokter - Dashboard</title>
 	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="../assets/css/font-awesome.min.css" rel="stylesheet">
 	<link href="../assets/css/datepicker3.css" rel="stylesheet">
@@ -35,10 +24,7 @@ if($_SESSION['level']!="admin"){
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span></button>
-				<a class="navbar-brand" href="#"><span>Dashboard</span>Dokter</a>
-				<ul class="nav navbar-top-links navbar-right">
-					
-				</ul>
+				<a class="navbar-brand" href="#"><span>SIPARTUS &nbsp;</span>Sistem Pakar Pendeteksi Ancaman Abortus</a>
 			</div>
 		</div><!-- /.container-fluid -->
 	</nav>
@@ -48,37 +34,22 @@ if($_SESSION['level']!="admin"){
 				<img src="../assets/img/profile.png" class="img-responsive" alt="">
 			</div>
 			<div class="profile-usertitle">
-				<div class="profile-usertitle-name"><?php echo $_SESSION['nama'] ?></div>
+				<div class="profile-usertitle-name">Username</div>
+				<div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
 			</div>
 			<div class="clear"></div>
 		</div>
 		<div class="divider"></div>
+		<form role="search">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+		</form>
 		<ul class="nav menu">
-        <form role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-					</form>
-			<li class="active"><a href="index.html"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-			<li><a href="#"><em class="fa fa-user">&nbsp;</em> Data User</a></li>
-			<li><a href="#"><em class="fa fa-bar-chart">&nbsp;</em> Penyakit</a></li>
-			<li><a href="#"><em class="fa fa-toggle-off">&nbsp;</em> Gejala</a></li>
-			<li><a href="#"><em class="fa fa-clone">&nbsp;</em> Laporan</a></li>
-			<li class="parent "><a data-toggle="collapse" href="#sub-item-1">
-				<em class="fa fa-navicon">&nbsp;</em> Multilevel <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
-				</a>
-				<ul class="children collapse" id="sub-item-1">
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 1
-					</a></li>
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 2
-					</a></li>
-					<li><a class="" href="#">
-						<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 3
-					</a></li>
-				</ul>
-			</li>
+			<li class="active"><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+			<li><a href="datauser.php"><em class="fa fa-user">&nbsp;</em> Data User</a></li>
+			<li><a href="gejala.php"><em class="fa fa-eye">&nbsp;</em> Gejala</a></li>
+			<li><a href="penyakit.php"><em class="fa fa-bug">&nbsp;</em> Penyakit</a></li>
 			<li><a href="../logout.php"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
 		</ul>
 	</div><!--/.sidebar-->
@@ -96,15 +67,77 @@ if($_SESSION['level']!="admin"){
 		<div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Dashboard</h1>
-			</div><!--/.col-->
-			
-			
-			
-			</div><!--/.col-->
-			<div class="col-sm-12">
-				<p class="back-link">Lumino Theme by <a href="https://www.medialoot.com">Medialoot</a></p>
 			</div>
 		</div><!--/.row-->
+		<?php 
+			require_once "../koneksi.php";
+			//menghitung jumlah user
+			$kueri = mysqli_query($koneksi, "SELECT * FROM user");
+ 
+ 			$data = array ();
+ 			while (($row = mysqli_fetch_array($kueri)) != null){
+ 			$data[] = $row;
+ 			}
+ 			$juser = count($data);
+
+ 			//menghitung jumlah gejala
+ 			$kueri2 = mysqli_query($koneksi, "SELECT * FROM gejala");
+ 
+ 			$data = array ();
+ 			while (($row = mysqli_fetch_array($kueri2)) != null){
+ 			$data[] = $row;
+ 			}
+ 			$jgejala = count($data);
+
+ 			//menghitung jumlah penyakit
+ 			$kueri3 = mysqli_query($koneksi, "SELECT * FROM penyakit");
+ 
+ 			$data = array ();
+ 			while (($row = mysqli_fetch_array($kueri3)) != null){
+ 			$data[] = $row;
+ 			}
+ 			$jpenyakit = count($data);
+
+ 			$jtotal = $juser+$jgejala+$jpenyakit;
+
+		 ?>
+		<div class="panel panel-container">
+			<div class="row">
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-teal panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-user color-blue"></em>
+							<div class="large"><?php echo $juser; ?></div>
+							<div class="text-muted">Total User</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-blue panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-eye color-orange"></em>
+							<div class="large"><?php echo $jgejala; ?></div>
+							<div class="text-muted">Total Gejala</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-orange panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-bug color-teal"></em>
+							<div class="large"><?php echo $jpenyakit ?></div>
+							<div class="text-muted">Total Penyakit</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-red panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-tasks color-red"></em>
+							<div class="large"><?php echo $jtotal; ?></div>
+							<div class="text-muted">Total Record</div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.row-->
+		</div>
+		
 	</div>	<!--/.main-->
 	
 	<script src="../assets/js/jquery-1.11.1.min.js"></script>
